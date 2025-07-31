c7ced01794e6f2941d1c6879b03c3bc8c17757fe
Kashaf Zia <kashafzia100@gmail.com>
2025-07-31 21:54:18 +0500

    Backend folder created and necessary setup done

---
+++ backend/utils/apiError.js
@@ -0,0 +1,13 @@
+class ApiError extends Error{
+    constructor(statusCode,message="Something went wrong",errors=[],stack=""){
+
+        super(message)
+        this.message=message
+        this.statusCode=statusCode
+        this.errors=errors
+        this.success=false
+
+    }
+}
+
+export default ApiError
